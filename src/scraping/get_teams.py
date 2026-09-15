f13d8ba3d3e450077e99be4fea14b5c154b6eea3
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-15 07:22:21 -0400

    wrote function to extract both teams from a match

---
+++ src/scraping/get_teams.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import get_soup
+
+def get_teams(match_url:str) -> list:
+    soup = get_soup(f"{match_url}")
+
+    teams = [team["href"].split("/")[-1].strip() for team in soup.select("#h1-title a")]
+
+    return teams
