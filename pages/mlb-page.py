fb747b33ca7e02e11f595f9acb812033ed168509
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-15 04:11:45 -0400

    MLB: added 'load' button with callback, added moneyline and SIERA inputs, all with state management

--- pages/mlb-page.py
+++ pages/mlb-page.py
@@ -9,3 +9,44 @@ st.header(
     body = "JavSport - MLB", 
     text_alignment = "center"
 )
+
+moneyline_col, siera_col = st.columns(2)
+
+with moneyline_col:
+    st.text_area(
+        "Enter Moneyline Data:",
+        key = "mlb_moneyline_data"
+    )
+    
+    st.button(
+        label = "Parse Moneyline Data",
+        width = "stretch"
+    )
+
+with siera_col:
+    st.text_area(
+        "Enter SIERA Data:",
+        key = "mlb_siera_data"
+    )
+    
+    st.button(
+        label = "Parse SIERA Data",
+        width = "stretch"
+    )
+
+if "mlb_load_button_text" not in st.session_state:
+    st.session_state["mlb_load_button_text"] = "Load MLB Wagers"
+
+def load_button_handler():
+    st.session_state["mlb_load_button_text"] = "Refresh MLB Wagers"
+    st.session_state["mlb_data"] = []
+
+with st.columns(3)[1]:
+    load_button = st.button(
+        label = st.session_state["mlb_load_button_text"],
+        width = "stretch",
+        on_click = load_button_handler
+    )
+
+if "mlb_data" in st.session_state:
+    "MLB Data Found"
