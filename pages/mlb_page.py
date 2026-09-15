6c6353c7b38530cf9e98913dde51bb3fa68fa55e
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-15 11:34:38 -0400

    loaded MLB data upon button click

--- pages/mlb_page.py
+++ pages/mlb_page.py
@@ -2,6 +2,9 @@ from datetime import datetime
 import streamlit as st
 from src.components import delay_disclaimer
 from src.components import timestamp_banner
+from src.data_collection.get_leaderboard import get_leaderboard
+from src.data_collection.get_schedule import get_schedule
+from src.data_collection.get_winloss import get_winloss
 from src.utils import format_timestamp
 
 st.set_page_config(
@@ -43,9 +46,50 @@ if "mlb_load_button_text" not in st.session_state:
 
 def load_button_handler():
     st.session_state["mlb_load_button_text"] = "Refresh MLB Wagers"
+
+    now = datetime.now()
+
     st.session_state["mlb_data"] = {
-        "timestamp": datetime.now()
+        "timestamp": now
     }
+
+    year, month, day = f"{now.year}", f"{now.month:02d}", f"{now.day:02d}"
+
+    schedule = get_schedule(f"https://www.teamrankings.com/mlb/schedules/?date={year}-{month}-{day}")
+    st.session_state["mlb_data"]["schedule"] = schedule
+
+    at_bats_per_game = get_leaderboard(
+        f"https://www.teamrankings.com/mlb/stat/at-bats-per-game?date={year}-{month}-{day}",
+        read_datasort = True
+    )
+    st.session_state["mlb_data"]["at_bats_per_game"] = at_bats_per_game
+
+    hits_per_game = get_leaderboard(
+        f"https://www.teamrankings.com/mlb/stat/hits-per-game?date={year}-{month}-{day}",
+        read_datasort = True
+    )
+    st.session_state["mlb_data"]["hits_per_game"] = hits_per_game
+
+    home_runs_per_game = get_leaderboard(
+        f"https://www.teamrankings.com/mlb/stat/home-runs-per-game?date={year}-{month}-{day}",
+        read_datasort = True
+    )
+    st.session_state["mlb_data"]["home_runs_per_game"] = home_runs_per_game
+
+    total_bases_per_game = get_leaderboard(
+        f"https://www.teamrankings.com/mlb/stat/total-bases-per-game?date={year}-{month}-{day}",
+        read_datasort = True
+    )
+    st.session_state["mlb_data"]["total_bases_per_game"] = total_bases_per_game
+
+    walks_per_game = get_leaderboard(
+        f"https://www.teamrankings.com/mlb/stat/walks-per-game?date={year}-{month}-{day}",
+        read_datasort = True
+    )
+    st.session_state["mlb_data"]["walks_per_game"] = walks_per_game
+
+    win_loss = get_winloss("https://www.teamrankings.com/mlb/trends/win_trends/")
+    st.session_state["mlb_data"]["win_loss"] = win_loss
 
 load_button_col = st.columns([1, 2, 1])[1]
 
@@ -62,5 +106,11 @@ if "mlb_data" in st.session_state:
 
     with load_button_col:
         timestamp_banner.render(timestamp_f)
+
+    for key in st.session_state["mlb_data"]:
+        key
+        st.write(st.session_state["mlb_data"][key])
+
+    st.divider()
 
 delay_disclaimer.render()
