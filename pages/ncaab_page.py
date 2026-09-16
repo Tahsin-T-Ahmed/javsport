95c04b7f477fc46bcc3f380237f2b4b777f5c27c
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-16 16:18:35 -0400

    added schedule-loads and session-state data rendering

--- pages/ncaab_page.py
+++ pages/ncaab_page.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import streamlit as st
 from src.components import delay_disclaimer
 from src.components import timestamp_banner
+from src.data_collection.get_leaderboard import get_leaderboard
+from src.data_collection.get_schedule import get_schedule
 from src.utils import format_timestamp
 
 st.set_page_config(
@@ -21,9 +23,25 @@ if "ncaab_load_button_text" not in st.session_state:
 
 def load_button_handler():
     st.session_state["ncaab_load_button_text"] = "Refresh NCAAB Wagers"
+
+    now = datetime.now()
+
     st.session_state["ncaab_data"] = {
-        "timestamp": datetime.now()
+        "timestamp": now
     }
+
+    schedule_map = get_schedule(
+        schedule_url = "https://www.teamrankings.com/ncb/schedules/",
+        timestamp = now
+    )
+
+    if schedule_map["error"]:
+        error = schedule_map["error"]
+        st.error(error)
+        return
+
+    schedule = schedule_map["content"]
+    st.session_state["ncaab_data"]["schedule"] = schedule
 
 with moneyline_col:
     st.text_area(
@@ -49,6 +67,10 @@ if "ncaab_data" in st.session_state:
 
     with load_button_col:
         timestamp_banner.render(timestamp_f)
+
+    for key, value in st.session_state["ncaab_data"].items():
+        key
+        value
 
     delay_disclaimer.render()
 else:
--- pages/wnba_page.py
+++ pages/wnba_page.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import streamlit as st
 from src.components import delay_disclaimer
 from src.components import timestamp_banner
+from src.data_collection.get_leaderboard import get_leaderboard
+from src.data_collection.get_schedule import get_schedule
 from src.utils import format_timestamp
 
 st.set_page_config(
@@ -21,9 +23,25 @@ if "wnba_load_button_text" not in st.session_state:
 
 def load_button_handler():
     st.session_state["wnba_load_button_text"] = "Refresh WNBA Wagers"
+
+    now = datetime.now()
+
     st.session_state["wnba_data"] = {
-        "timestamp": datetime.now()
+        "timestamp": now
     }
+
+    schedule_map = get_schedule(
+        schedule_url = "https://www.teamrankings.com/wnba/schedules/",
+        timestamp = now
+    )
+
+    if schedule_map["error"]:
+        error = schedule_map["error"]
+        st.error(error)
+        return
+
+    schedule = schedule_map["content"]
+    st.session_state["wnba_data"]["schedule"] = schedule
 
 with moneyline_col:
     st.text_area(
@@ -49,6 +67,10 @@ if "wnba_data" in st.session_state:
 
     with load_button_col:
         timestamp_banner.render(timestamp_f)
+
+    for key, value in st.session_state["wnba_data"].items():
+        key
+        value
 
     delay_disclaimer.render()
 else:
