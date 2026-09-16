1d4a248cc7e91f77d31837a2e084083ebddd93b1
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-16 13:15:37 -0400

    REFACTOR (get_leaderboard): implemented scan_table with error-check, incorporated timestamp, filtered desired columns, formatted column names

--- src/data_collection/get_leaderboard.py
+++ src/data_collection/get_leaderboard.py
@@ -1,77 +1,44 @@
-from src.data_collection.get_soup import get_soup
+from datetime import datetime
 import pandas as pd
 from src.data_collection.data_maps import DataFrameMap
+from src.data_collection.scan_table import scan_table
 
-def get_leaderboard(leaderboard_url:str, read_datasort:bool = False) -> DataFrameMap:
-    soup_map = get_soup(leaderboard_url)
-    if soup_map["error"]:
+def get_leaderboard(leaderboard_url:str, timestamp: datetime.datetime) -> DataFrameMap:
+    year, month, day = f"{timestamp.year:04d}", f"{timestamp.month:02d}", f"{timestamp.day:02d}"
+    date_str = f"{year}-{month}-{day}"
+
+    leaderboard_map = scan_table(f"{leaderboard_url}?date={date_str}")
+    if leaderboard_map["error"]:
         return dict(
-            error = soup_map["error"],
+            error = leaderboard_map["error"],
             content = None
         )
 
-    soup = soup_map["content"]
+    leaderboard = leaderboard_map["content"]
 
-    headers_raw = soup.find_all("th")
-    if not headers_raw:
-        return dict(
-            error = f"ERROR (Leaderboard): Failed to scan HEADERS from URL ({leaderboard_url})",
-            content = None
-        )
-    headers = [header.text.upper() for header in headers_raw]
+    leaderboard["TEAM ID"] = leaderboard["TEAM_LINK"].apply(
+        lambda link: link.split("/")[-1]
+    )
 
-    rows = soup.find_all("tr")
-    if not rows:
-        return dict(
-            error = f"ERROR (Leaderboard): Failed to scan ROWS from URL ({leaderboard_url})",
-            content = None
-        )
-    
-    headers[-1] = "PAST"
-    
-    df = pd.DataFrame(columns = headers)
+    leaderboard.drop(
+        columns = [
+            *leaderboard.columns[:9],
+            leaderboard.columns[10]
+        ],
+        inplace = True
+    )
 
-    for i, row in enumerate(rows):
-        if 0 == i:
-            continue
+    leaderboard.columns = [column.replace("_DATASORT", "") for column in leaderboard.columns]
 
-        cells = row.find_all("td")
-        if not cells:
-            return dict(
-                error = f"ERROR (Leaderboard): Failed to scan CELLS of row ({row}) from URL ({leaderboard_url})",
-                content = False
-            )
-
-        new_row_idx = df.shape[0]
-
-        for col_idx, col in enumerate(headers):
-            cell = cells[col_idx]
-            cell_content = cell.text
-
-            if headers.index("TEAM") == col_idx:
-                team_link = cell.find("a")
-                if not team_link:
-                    return dict(
-                        error = f"ERROR (Leaderboard): Failed to scan TEAM-LINK of cell ({cell}) from URL ({leaderboard_url})",
-                        content = None
-                    )
-                
-                cell_content = team_link["href"].split("/")[-1]
-            else:
-                if read_datasort:
-                    cell_content = cell["data-sort"]
-
-            df.loc[new_row_idx, col] = cell_content.strip()
-
-    df.rename(
+    leaderboard.rename(
         columns = {
-            "TEAM": "TEAM ID",
-            df.columns[2]: "NOW"
-        }, 
+            leaderboard.columns[1]: "NOW",
+            leaderboard.columns[-2]: "PAST"
+        },
         inplace = True
     )
     
     return dict(
         error = None,
-        content = df
+        content = leaderboard
     )
