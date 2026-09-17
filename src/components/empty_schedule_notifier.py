6d1074013142c6ffe083de2332e36efc6a64f9b2
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-17 16:33:49 -0400

    added weekday to empty-schedule notifier

--- src/components/empty_schedule_notifier.py
+++ src/components/empty_schedule_notifier.py
@@ -3,7 +3,10 @@ import streamlit as st
 from src.utils.get_date_ordinal_suffix import get_date_ordinal_suffix
 
 def render(sport_name: str, timestamp: datetime.datetime):
-        year, month, day = timestamp.year, timestamp.strftime('%B'), timestamp.day
+        year = timestamp.year
+        month = timestamp.strftime('%B')
+        day = timestamp.day
+        weekday = timestamp.strftime("%A")
         day_ordinal_suffix = get_date_ordinal_suffix(day)
         
-        st.info(f"No {sport_name} games scheduled for {month} {day}{day_ordinal_suffix}, {year}")
+        st.info(f"No {sport_name} games scheduled for {weekday}, {month} {day}{day_ordinal_suffix}, {year}")
