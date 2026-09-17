bcef04d252b69befc62e9e68329f9b663ab477a3
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-17 16:08:42 -0400

    wrote getter function (service) to get all data for a sport (schedules, leaderboards, etc)

---
+++ src/services/get_sport_data.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from src.data_collection.data_maps import DictMap
+from src.data_collection.parsers.make_schedule import make_schedule
+from src.services.get_leaderboards import get_leaderboards
+
+def get_sport_data(
+    timestamp: datetime.datetime,
+    schedule_url: str,
+    leaderboard_urls_dict: dict,
+    winloss_url: str | None = None
+) -> DictMap:
+    sport_data_dict = dict()
+
+    schedule_map = make_schedule(
+        schedule_url = schedule_url,
+        timestamp = timestamp
+    )
+
+    if schedule_map["error"]:
+        return dict(
+            error = schedule_map["error"],
+            content = None
+        )
+
+    schedule = schedule_map["content"]
+    sport_data_dict["schedule"] = schedule
+
+    leaderboards_map = None
+
+    if winloss_url:
+        leaderboards_map = get_leaderboards(
+            timestamp = timestamp,
+            leaderboard_urls_dict = leaderboard_urls_dict,
+            winloss_url = winloss_url
+        )
+    
+    else:
+        leaderboards_map = get_leaderboards(
+            leaderboard_urls_dict = leaderboard_urls_dict,
+            timestamp = timestamp
+        )
+
+    if leaderboards_map["error"]:
+        return dict(
+            error = leaderboards_map["error"],
+            content = None
+        )
+
+    leaderboards = leaderboards_map["content"]
+    sport_data_dict["leaderboards"] = leaderboards
+
+    return dict(
+        error = None,
+        content = sport_data_dict
+    )
