c882f44ab62b5ede9c08e179b9c4210f61d99837
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-17 08:58:03 -0400

    allowed day-of-month to be single-digit in timestamp-formatter

--- src/utils/format_timestamp.py
+++ src/utils/format_timestamp.py
@@ -2,13 +2,17 @@ from datetime import datetime
 from src.utils.get_date_ordinal_suffix import get_date_ordinal_suffix
 
 def format_timestamp(timestamp) -> dict:
-    date = timestamp.strftime('%Y, %B %d')
+    year = f"{timestamp.year:04d}"
+    month_name = timestamp.strftime("%B")
+    day = timestamp.day
+
+    date_str = f"{year}, {month_name} {day}"
     clocktime = timestamp.strftime('%I:%M:%S %p')
     timezone = timestamp.astimezone().tzname()
     ordinal_suffix = get_date_ordinal_suffix(timestamp.day)
 
     return {
-        "date": f"{date}{ordinal_suffix}",
+        "date": f"{date_str}{ordinal_suffix}",
         "clocktime": clocktime,
         "timezone": timezone
     }
