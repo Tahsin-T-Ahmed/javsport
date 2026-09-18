6bdbe5616fd8c706537ebd4328837cbbb468763c
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-18 17:10:25 -0400

    removed anchors from title and header on sport game

--- src/components/sport_wagers_page.py
+++ src/components/sport_wagers_page.py
@@ -46,12 +46,14 @@ def render(
 
     st.header(
         body = f"JavSport - {sport_title}",
-        text_alignment = "center"
+        text_alignment = "center",
+        anchor = False
     )
 
     st.markdown(
         body = f"#### {sport_icon} {sport_subheader} {sport_icon}",
-        text_alignment = "center"
+        text_alignment = "center",
+        anchors = False
     )
 
     st.divider()
