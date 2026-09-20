cfd6aa514af79ef92aec082790ae5c43645ac746
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-20 08:06:32 -0400

    wrote dedicated schedule-getter function to isolate from other data, since it influences high-level logic

---
+++ src/services/get_schedule.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from src.data_collection.data_maps import DictMap
+from src.data_collection.parsers.make_schedule import make_schedule
+
+def get_schedule(
+    timestamp: datetime.datetime,
+    schedule_url: str
+) -> DictMap:
+    schedule_dict = dict()
+
+    schedule_map = make_schedule(
+        schedule_url = schedule_url,
+        timestamp = timestamp
+    )
+
+    if schedule_map["error"]:
+        return dict(
+            error = schedule_map["error"],
+            content = None
+        )
+
+    schedule = schedule_map["content"]
+    schedule_dict["data"] = schedule
+
+    if schedule.empty:
+        return dict(
+            error = None,
+            content = schedule_dict
+        )
+
+    schedule_display = schedule.copy()
+    schedule_display["TIME"] = schedule_display["TIME"].dt.strftime("%I:%m %p")
+
+    schedule_dict["display"] = schedule_display
+
+    return schedule_dict
