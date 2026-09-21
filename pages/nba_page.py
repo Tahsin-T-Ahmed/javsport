11bfcc29533c06238336b8e007886a9f1a93cffb
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-21 17:43:58 -0400

    added '_per_game' to leaderboard names in basketball sport-pages (NBA, NCAAB, WNBA), changed leaderboard name order to alphabetical order

--- pages/nba_page.py
+++ pages/nba_page.py
@@ -6,11 +6,11 @@ sport_wagers_page.render(
     sport_icon=":material/sports_basketball:",
     schedule_url="https://www.teamrankings.com/nba/schedules/season/?week=0",
     leaderboard_urls_dict=dict(
-        field_goals_attempted="https://www.teamrankings.com/nba/stat/field-goals-attempted-per-game",
-        field_goals_made="https://www.teamrankings.com/nba/stat/field-goals-made-per-game",
-        free_throws_made="https://www.teamrankings.com/nba/stat/free-throws-made-per-game",
-        three_pointers_made="https://www.teamrankings.com/nba/stat/three-pointers-made-per-game",
-        points_per_game="https://www.teamrankings.com/nba/stat/points-per-game"
+        field_goals_attempted_per_game="https://www.teamrankings.com/nba/stat/field-goals-attempted-per-game",
+        field_goals_made_per_game="https://www.teamrankings.com/nba/stat/field-goals-made-per-game",
+        free_throws_made_per_game="https://www.teamrankings.com/nba/stat/free-throws-made-per-game",
+        points_per_game="https://www.teamrankings.com/nba/stat/points-per-game",
+        three_pointers_made_per_game="https://www.teamrankings.com/nba/stat/three-pointers-made-per-game"
     ),
     win_trends_url="https://www.teamrankings.com/nba/trends/win_trends/"
 )
