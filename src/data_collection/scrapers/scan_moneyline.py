bd5eb6ff60de88ea5a28e64cc9e63b10d66e3682
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-24 20:05:59 -0400

    wrote function for scanning moneyline table of given timestamp from .mhtml file

--- src/data_collection/scrapers/scan_moneyline.py
+++ src/data_collection/scrapers/scan_moneyline.py
@@ -20,9 +20,70 @@ def scan_moneyline(
     soup = BeautifulSoup(file)
 
     modules = soup.select("div[class*=module]:not([class*=ajax]):not([class*=-in])")
+    if not modules:
+        return DataFrameMap(
+            error=f"ERROR (Moneyline-Scanner): No module-divs found in Moneyline page.",
+            content=None
+        )
+
+    moneyline_df = pd.DataFrame()
 
     for module in modules:
-        date_header = module.find("h2").text
-        st.write(f"Date header: {date_header}")
-        st.write(f"Date string: {date_str}")
-        st.write(f"Equal? {date_header == date_str}")
+        module_header = module.find("h2")
+        if not module_header:
+            continue
+
+        module_date = module_header.text
+
+        if module_date != date_str:
+            continue
+
+        subtables = module.find_all("table")
+        if not subtables:
+            continue
+        
+        for subtable in subtables:
+            headers = subtable.find_all("th")
+            if not headers:
+                continue
+            
+            columns = [header.text.strip().upper() if ":" not in header.text else "TEAM" for header in headers]
+            n_columns = len(columns)
+
+            tbody = subtable.find("tbody")
+            if not tbody:
+                continue
+
+            rows = tbody.find_all("tr")
+            if not rows:
+                continue
+
+            subtable_df = pd.DataFrame()
+
+            for row in rows:
+                cells = row.find_all("td")
+                if not cells:
+                    continue
+
+                if len(cells) < n_columns:
+                    continue
+
+                new_row_idx = subtable_df.shape[0]
+
+                for cell, column in zip(cells, columns):
+                    subtable_df.loc[new_row_idx, column] = cell.text
+
+            subtable_df["TOTAL"] = subtable_df["TOTAL"].sum()
+
+            moneyline_df = pd.concat(
+                [moneyline_df, subtable_df],
+                axis=0
+            )
+
+    moneyline_df.reset_index(drop=True, inplace=True)
+    st.write(moneyline_df)
+
+    return DataFrameMap(
+        error=None,
+        content=moneyline_df
+    )
