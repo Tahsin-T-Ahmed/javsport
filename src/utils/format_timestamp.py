298c86d6412f4ccadb8d6fe3542bfab8b6a15df9
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-24 20:36:03 -0400

    added param type 'datetime' to timestamp-formatter util

--- src/utils/format_timestamp.py
+++ src/utils/format_timestamp.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+import datetime
 from src.utils.get_date_ordinal_suffix import get_date_ordinal_suffix
 
-def format_timestamp(timestamp) -> dict:
+def format_timestamp(timestamp: datetime.datetime) -> dict:
     year = f"{timestamp.year:04d}"
     month_name = timestamp.strftime("%B")
     day = timestamp.day
