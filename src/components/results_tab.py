2daf32b9b1e44f314bc90b89726aa54c229c8b6d
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-09-26 11:26:59 -0400

    moved file-requirement-fulfillment to results-tab component, from upload-dialog

--- src/components/results_tab.py
+++ src/components/results_tab.py
@@ -7,17 +7,25 @@ def render(
     sport_title: str,
     required_files_list: list[RequiredFileMap]
 ):
-    if required_files_list:
-        for file_map in required_files_list:
-            upload_dialog.render(
-                **file_map,
-                sport_key=sport_key,
-                sport_title=sport_title,
-                timestamp=st.session_state[sport_key]["timestamp"]
-            )
+    if not required_files_list:
+        st.session_state[sport_key]["file_requirements"] = dict(
+            fulfilled=True
+        )
 
         return
-    
-    st.session_state[sport_key]["file_requirements"] = dict(
-        fulfilled=True
-    )
+
+    for file_map in required_files_list:
+        upload_dialog.render(
+            **file_map,
+            sport_key=sport_key,
+            sport_title=sport_title,
+            timestamp=st.session_state[sport_key]["timestamp"]
+        )        
+
+    if all(
+        st.session_state[sport_key]["file_requirements"]["data"][file] is not None
+        for file in st.session_state[sport_key]["file_requirements"]["data"]
+    ):
+        st.session_state[sport_key]["file_requirements"]["fulfilled"] = True
+    else:
+        st.session_state[sport_key]["file_requirements"]["fulfilled"] = False
